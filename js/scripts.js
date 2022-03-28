9d17ab91b317109c8bbff6a136b9f3bbab781308
Jarret Jeter <jarretjeter@yahoo.com>
2022-03-28 16:25:05 -0700

    add functionality to build an array of cards
    co-authored-by: Jake Edgar <jakeedgar1012@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
 $(document).ready(function() {
-  
-  $("h1").click(function() {
-    alert("This is a heading.");
+  $("#formOne").submit(function(event) {
+  event.preventDefault();
+  let cards = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"];
+  let suits = ["of clubs", "of spades", "of diamonds", "of hearts"];
+  let deck = [];
+  console.log(cards);
+  console.log(suits);
+  console.log(deck);
+
+  suits.forEach(function(suit) {
+    cards.forEach(function(card) {
+      deck.push(" " + card + " " + suit);
+    });
   });
 
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
+  $("#output").text(deck.toString().toUpperCase());
+  console.log(deck);
 
-  $("img").click(function() {
-    alert("This is an image.");
+  //   suits.forEach(function(suit) {
+  //   cards.forEach(function(card) {
+  //     $("<li></li>").append(deck.push(card + " " + suit));
+  //   });
+  // });
+
+  // // $("#output").text(deck);
+  // $("<li></li>").text(deck);
   });
 });
-
-
-
